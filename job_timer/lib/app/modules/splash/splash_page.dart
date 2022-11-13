7d42b78bfb757db3cd039e5654dae86932f9b911
laerthnardelli <laerthnardelli@gmail.com>
2022-11-13 16:28:59 -0300

    Add SplashPage

--- job_timer/lib/app/modules/splash/splash_page.dart
+++ job_timer/lib/app/modules/splash/splash_page.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:job_timer/app/core/ui/job_timer_icons.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashPage'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Input'),
-              ),
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0XFF0092B9),
+              Color(0XFF0167B2),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('BOTÃO'),
-          ),
-          const Icon(
-            JobTimerIcons.angle_double_right,
-            size: 50,
-          ),
-        ],
+        ),
+        child: Center(
+          child: Image.asset('assets/images/logo.png'),
+        ),
       ),
     );
   }
