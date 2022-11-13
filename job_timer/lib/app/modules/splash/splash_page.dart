6bb21e9d312ddd516acb21ee02087ba6feebd18a
laerthnardelli <laerthnardelli@gmail.com>
2022-11-13 16:47:56 -0300

    added  logic Firebase Auth in SplashPage

--- job_timer/lib/app/modules/splash/splash_page.dart
+++ job_timer/lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -8,6 +10,20 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen(
+      (User? user) {
+        if (user == null) {
+          Modular.to.navigate('/login');
+        } else {
+          Modular.to.navigate('/home');
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
