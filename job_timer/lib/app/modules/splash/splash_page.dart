f68527876bf31ea113e9588c0fe43df3a3bf0f8b
laerthnardelli <laerthnardelli@gmail.com>
2022-11-13 16:09:36 -0300

    Add config Icons and Assets

--- job_timer/lib/app/modules/splash/splash_page.dart
+++ job_timer/lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/app/core/ui/job_timer_icons.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,7 +10,27 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('SplashPage'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Container(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Input'),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('BOTÃO'),
+          ),
+          const Icon(
+            JobTimerIcons.angle_double_right,
+            size: 50,
+          ),
+        ],
+      ),
     );
   }
 }
