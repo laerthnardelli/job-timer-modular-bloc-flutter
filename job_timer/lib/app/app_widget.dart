8ef70272b3cf35788f5c92d4815e6be968a17696
laerthnardelli <laerthnardelli@gmail.com>
2022-11-13 15:41:30 -0300

    added asuka config

--- job_timer/lib/app/app_widget.dart
+++ job_timer/lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Job Timer',
+      builder: asuka.builder,
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
