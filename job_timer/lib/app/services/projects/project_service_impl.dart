c0e8f5b59a7192ee9f1ae8860133a5a80524bf40
laerthnardelli <laerthnardelli@gmail.com>
2022-12-19 12:30:45 -0300

    add ProjectService and ProjectServiceImpl

---
+++ job_timer/lib/app/services/projects/project_service_impl.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:job_timer/app/core/entities/project.dart';
+import 'package:job_timer/app/repositories/projects/project_repository.dart';
+import 'package:job_timer/app/view_models/project_model.dart';
+
+import './project_service.dart';
+
+class ProjectServiceImpl implements ProjectService {
+  final ProjectRepository _projectRepository;
+
+  ProjectServiceImpl({
+    required projectRepository,
+  }) : _projectRepository = projectRepository;
+
+  @override
+  Future<void> register(ProjectModel projectModel) async {
+    //Tradução, de projectModel(view_model) para Project(entidade)
+    // final project = Project();
+    //   project.id = projectModel.id;
+
+    //cascade notation
+    final project = Project()
+      ..id = projectModel.id
+      ..name = projectModel.name
+      ..status = projectModel.status
+      ..estimate = projectModel.estimate;
+
+    await _projectRepository.register(project);
+  }
+}
