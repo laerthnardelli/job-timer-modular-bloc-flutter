8ee67f06f9b5636499a470dda2f0889362757b70
laerthnardelli <laerthnardelli@gmail.com>
2022-12-21 20:19:25 -0300

    add task controller, state, and layout TaskPage

--- job_timer/lib/app/modules/project/task/task_module.dart
+++ job_timer/lib/app/modules/project/task/task_module.dart
@@ -1,16 +1,25 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/project/task/controller/task_controller.dart';
 import 'package:job_timer/app/modules/project/task/task_page.dart';
+import 'package:modular_bloc_bind/modular_bloc_bind.dart';
 
 import '../../../view_models/project_model.dart';
 
 class TaskModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [
+        BlocBind.lazySingleton((i) => TaskController()),
+      ];
+
   @override
   List<ModularRoute> get routes => [
         ChildRoute(
           '/',
           child: (context, args) {
             final ProjectModel projectModel = args.data;
-            return const TaskPage();
+            return TaskPage(
+              controller: Modular.get()..setProject(projectModel),
+            );
           },
         ),
       ];
--- job_timer/lib/app/modules/project/task/task_page.dart
+++ job_timer/lib/app/modules/project/task/task_page.dart
@@ -1,15 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class TaskPage extends StatelessWidget {
-  const TaskPage({super.key});
+import 'controller/task_controller.dart';
+
+class TaskPage extends StatefulWidget {
+  final TaskController controller;
+
+  const TaskPage({super.key, required this.controller});
+
+  @override
+  State<TaskPage> createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameEC = TextEditingController();
+  final _durationEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _durationEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Criar nova task'),
+        title: const Text(
+          'Criar nova task',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
+        backgroundColor: Colors.white,
       ),
-      body: Container(),
+      backgroundColor: Colors.white,
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameEC,
+                decoration: const InputDecoration(
+                  label: Text('Nome da task'),
+                ),
+                validator: Validatorless.required('Nome obrigatório'),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _durationEC,
+                decoration: const InputDecoration(
+                  label: Text('Duração da task'),
+                ),
+                keyboardType: TextInputType.number,
+                validator: Validatorless.multiple([
+                  Validatorless.required('Duração obrigatório'),
+                  Validatorless.number('Permetido somente números'),
+                ]),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Salvar'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
