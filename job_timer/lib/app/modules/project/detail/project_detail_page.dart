b0e38de41ea9ed42b345a18e781b7dfe77eba097
laerthnardelli <laerthnardelli@gmail.com>
2022-12-20 21:22:48 -0300

    add ProjectTaskTile

--- job_timer/lib/app/modules/project/detail/project_detail_page.dart
+++ job_timer/lib/app/modules/project/detail/project_detail_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/ui/job_timer_icons.dart';
 import 'widgets/project_detail_appbar.dart';
 import 'widgets/project_pie_chart.dart';
+import 'widgets/project_task_tile.dart';
 
 class ProjectDetailPage extends StatelessWidget {
   const ProjectDetailPage({super.key});
@@ -19,9 +21,25 @@ class ProjectDetailPage extends StatelessWidget {
                   padding: EdgeInsets.only(top: 50.0, bottom: 50),
                   child: ProjectPieChart(),
                 ),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
               ],
             ),
           ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(JobTimerIcons.ok_circled2),
+                    label: const Text('Finalizar Projeto')),
+              ),
+            ),
+          )
         ],
       ),
     );
